66f9bbd670c6e7171b8663688baee20570f14189
Jerry xu <Jerry.x@outlook.com>
2024-06-02 22:15:00 +0800

    ✨init commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# Docs-Guides
+
+使用 [Docusaurus](https://docusaurus.io/) 构建的文档手册静态页面示例项目，其他项目可使用该模板进行二次修改使用
+
+## 要求
+
+- [Node.js](https://nodejs.org/en/download): 18.0+
+- [Yarn](https://yarn.nodejs.cn/en/docs/install#windows-stable): 1.20+
+
+## 项目结构
+
+```text
+docs-guides
+  ├── blog                          # 博客文章目录
+  │   └── 2020-05-30-hola.md
+  ├── docs                          # 文档目录
+  │   ├── doc1.md
+  │   ├── doc2.md
+  │   ├── doc3.md
+  │   └── mdx.md
+  ├── src                           # 页面或自定义的 React 组件目录
+  │   ├── css
+  │   │   └── custom.css
+  │   └── pages
+  │       ├── styles.module.css
+  │       └── index.js
+  ├── static                        # 静态文件目录
+  │   └── img
+  ├── i18n
+  │   └── zh-Hans                   # 简体中文翻译位置
+  │       ├── docusaurus-plugin-content-docs
+  │       │   │── current
+  │       │   │   ├── ...           # 要与 docs 目录结构相同的文件翻译
+  │       │   │   └── doc1.md
+  │       │   └── current.json      # 侧边栏
+  │       ├── docusaurus-theme-classic
+  │       │   └── navbar.json       # 导航栏
+  │       ├── docusaurus-plugin-content-blog
+  │       │   ├── ...               # 要与 blog 目录结构相同的文件翻译
+  │       │   └── doc1.md
+  │       └── code.json             # 定义了 React 代码中使用的所有文本标签
+  ├── babel.config.js
+  ├── docusaurus.config.ts          # 配置文件目录
+  ├── package.json                  # 项目依赖管理
+  ├── README.md                     # 项目解释 README
+  ├── sidebars.ts                   # 文档侧边栏配置文件
+  └── tsconfig.json
+```
+
+## 相关操作
+
+```bash
+# clone 项目
+git clone https://github.com/RootCluster/Docs-Guides.git
+# 
+cd docs-guides
+# 安装依赖
+yarn install
+
+# 按需调整配置（查看下一节）
+
+# 本地预览, 默认英文
+yarn start
+# 指定语言预览
+yarn run start --locale zh-Hans
+# build
+yarn build
+
+
+# 如何升级
+
+```
+
+## 配置调整
+
+```ts
+// 项目子路径
+baseUrl: ''
+```
+
+## 路线图
+
+- [x] 简单示例
+- [ ] 插件集成
+  - [ ] 图片预览
+  - [ ] 本地搜索
+  - [ ] 图表支持
+- [ ] 打包脚本增强
+- [ ] 分版
+- [ ] 离线 PDF 生成
+
+## 其他
+
+- 搜索功能
+  - 暂时先使用 [docusaurus-search-local](https://github.com/easyops-cn/docusaurus-search-local)
+  - 等待 [issues-205](https://github.com/cmfcmf/docusaurus-search-local/issues/205)
+- 离线PDF
+  - [docusaurus-prince-pdf](https://github.com/signcl/docusaurus-prince-pdf)
---
+++ docusaurus.config.ts
@@ -0,0 +1,173 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Docs-Guides',
+  tagline: 'Docs Demo',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: 'guide/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en','zh-Hans'],
+  },
+
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          // https://docusaurus.io/zh-CN/docs/api/plugins/@docusaurus/plugin-content-docs
+          sidebarPath: './sidebars.ts',
+          path: "docs",
+          routeBasePath: "/",
+        },
+        blog: {
+          // https://docusaurus.io/zh-CN/docs/api/plugins/@docusaurus/plugin-content-blog
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
+    },
+    docs: {
+      sidebar: {
+        hideable: true,
+        autoCollapseCategories: true,
+      },
+    },
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Docs Guides',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+        srcDark: 'img/logo_dark.svg',
+        href: '/intro'
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          type: 'search',
+          position: 'right',
+        },
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+          dropdownActiveClassDisabled: true,
+          dropdownItemsAfter: [
+            {
+              to: '/versions',
+              label: 'All versions',
+            },
+          ],
+        },
+        // language change
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Tutorial',
+      //         to: '/docs/intro',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'Community',
+      //     items: [
+      //       {
+      //         label: 'Stack Overflow',
+      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //       },
+      //       {
+      //         label: 'Discord',
+      //         href: 'https://discordapp.com/invite/docusaurus',
+      //       },
+      //       {
+      //         label: 'Twitter',
+      //         href: 'https://twitter.com/docusaurus',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'More',
+      //     items: [
+      //       {
+      //         label: 'Blog',
+      //         to: '/blog',
+      //       },
+      //       {
+      //         label: 'GitHub',
+      //         href: 'https://github.com/facebook/docusaurus',
+      //       },
+      //     ],
+      //   },
+      // ],
+      copyright: `Copyright © ${new Date().getFullYear()} Docs Guides, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
